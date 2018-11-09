a7c04192f72bbd4ff8cf0ac74ae7904d6f3c01bb
LaNika Dillon <ladillon@microsoft.com>
2018-11-09 07:53:37 -0500

    Added regex hash tables

--- Module/RegistryRule/Data.ps1
+++ Module/RegistryRule/Data.ps1
@@ -54,3 +54,70 @@ data dscRegistryValueType
         Enabled        = Dword
 '@
 }
+
+data testExpression
+{
+    ConvertFrom-StringData -stringdata @'
+    Red = Apple
+    Yellow = Banana
+'@
+}
+
+$SingleLineRegistryPath = 
+     [ordered]@{
+        Root     = [ordered]@{ 
+                        Match    = '(HKCU|HKLM|HKEY_LOCAL_MACHINE)\\'; 
+                        Select   = '((HKLM|HKCU|HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER).*)' 
+                    };
+
+        Criteria = [ordered]@{ 
+                        Contains = 'Criteria:'; 
+                        After    = [ordered]@{ 
+                                        Match  = '((HKLM|HKCU).*(?=Criteria:))';
+                                        Select = '((HKLM|HKCU).*(?=Criteria:))'; 
+                                        
+                                    };
+                        Before   = [ordered]@{
+                                        Match = 'Criteria:.*(HKLM|HKCU)'
+                                        Select = '((HKLM|HKCU).*(?=\sis))'
+                                    } 
+                    };
+
+        Verify = [ordered]@{ 
+                        Contains = 'Verify'; 
+                        Select   = '((HKLM|HKCU).*(?=Verify))'
+                  };
+    }
+
+$SingleLineRegistryValueName = 
+     [ordered]@{
+     One = @{ Select = '(?<=If the value(\s*)?((for( )?)?)").*(")?((?=is.*R)|(?=does not exist))' };
+     Two = [ordered]@{ Match = 'If the.+(registry key does not exist)'; Select = '"[\s\S]*?"' };
+     Three = @{ Select = '(?<=If the value of\s")(.*)(?="\s.*R)|(?=does not exist)' };
+     Four = @{ Select = '((?<=If the value\s)(.*)(?=is\sR))' };
+     Five = [ordered]@{ Match = 'the policy value'; Select = '(?<=")(.*)(?="\sis)' };
+     Six = @{ Select = '((?<=for\s).*)' };
+     Seven = @{ Select = '(?<=filevalidation\\).*(?=\sis\sset\sto)' }
+     }
+
+$SingleLineRegistryValueType = 
+     [ordered]@{
+     One = @{ Select = '(?<={0}(\"")? is not ).*=' }; #$([regex]::escape($myString))
+     Two = @{ Select =  '(?<="{0}"\sis).*='}; #'(?<={0}(\"")?\s+is ).*=' }; 
+     #'(?<={0}(\"")?\s+is ).*=' };
+     Three = @{ Select = '(?<=Verify\sa).*(?=value\sof)'};
+     #Four = @{ Select = 'registry key exists and the([\s\S]*?)value'; Group = 1 };
+     Five = @{ Select = '(?<={0}`" is set to ).*`"'};
+     #Six = @{ Select = '((hkcu|hklm).*\sis\s(.*)=)'; Group = 3 };
+     #Seven = @{ Select = 'does not exist, this is not a finding'; Return = 'Does Not Exist'}
+     }
+
+$SingleLineRegistryValueData = 
+     [ordered]@{
+     One = @{ Select = '(?<={0}(\s*)?=).*(?=(,|\())' };
+     Two = @{ Select = '((?<=value\sof).*(?=for))' };
+     Three = @{ Select = '((?<=set\sto).*(?=\(true\)))' };
+     Four = @{ Select = "((?<=is\sset\sto\s)(`'|`")).*(?=(`'|`"))" };
+     Five = @{ Select = "(?<={0}\s=).*"}
+     }
+
